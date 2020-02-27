22169c0c4daff0ef527c3fb48e9d66766f3497d2
FelipePineda <henaopipepineda@gmail.com>
2020-02-27 02:39:48 -0500

    Se añadio una funcion llamada scrolltobottom

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ document.getElementById('hora').innerHTML = "la hora es:  " + d.getHours() + ":"
 
 var time = new Date();
 console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+
+$(document).ready(function() {
+
+    $('.scrollToBottom').click(function() {
+        $('html, body').animate({ scrollTop: $(document).height() }, 1000);
+
+        return false;
+
+    });
+
+});
