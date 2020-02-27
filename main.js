452b91a6bf69373fb2203144a53de43d2a068ec9
FelipePineda <henaopipepineda@gmail.com>
2020-02-26 23:58:57 -0500

    codigo base

---
+++ main.js
@@ -0,0 +1,5 @@
+let d = new Date();
+document.getElementById('hora').innerHTML = "la hora es:  " + d.getHours() + ":" + d.getMinutes();
+
+var time = new Date();
+console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
